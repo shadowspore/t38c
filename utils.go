8d2b1bab3e76aa11f55e12295e300930ee28db0a
zerobounty <yuiop1955@mail.ru>
2020-04-05 20:30:15 +0500

    refactoring

--- utils.go
+++ utils.go
@@ -2,8 +2,22 @@ package t38c
 
 import (
 	"strconv"
+
+	geojson "github.com/paulmach/go.geojson"
+	"github.com/tidwall/gjson"
 )
 
 func floatToString(val float64) string {
 	return strconv.FormatFloat(val, 'f', 6, 64)
+}
+
+func unmarshalGeoJSON(data []byte) (interface{}, error) {
+	switch gjson.GetBytes(data, "type").String() {
+	case "FeatureCollection":
+		return geojson.UnmarshalFeatureCollection(data)
+	case "Feature":
+		return geojson.UnmarshalFeature(data)
+	default:
+		return geojson.UnmarshalGeometry(data)
+	}
 }
