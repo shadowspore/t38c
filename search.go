363ef641a651af1cc0d156f41e963423f6510f51
zerobounty <yuiop1955@mail.ru>
2020-04-05 19:41:26 +0500

    refactoring

--- search.go
+++ search.go
@@ -4,24 +4,29 @@ import (
 	"strings"
 )
 
-type searchResponse struct {
-	Fields  []string `json:"fields"`
-	Objects []struct {
-		ID     string  `json:"id"`
-		Object *Object `json:"object"`
-		Fields []int   `json:"fields"`
-	} `json:"objects"`
-}
+func (client *Tile38Client) searchObjects(cmd, key, area string, opts []SearchOption) ([]*Object, error) {
+	var resp struct {
+		Fields  []string `json:"fields"`
+		Objects []struct {
+			ID     string  `json:"id"`
+			Object *Object `json:"object"`
+			Fields []int   `json:"fields"`
+		} `json:"objects"`
+	}
 
-func (sr searchResponse) getObjects() []*Object {
-	objects := make([]*Object, len(sr.Objects))
-	haveFields := len(sr.Fields) > 0
-	for idx, obj := range sr.Objects {
+	command := buildSearchCommand(cmd, key, area, opts)
+	if err := client.execute(command, &resp); err != nil {
+		return nil, err
+	}
+
+	objects := make([]*Object, len(resp.Objects))
+	haveFields := len(resp.Fields) > 0
+	for idx, obj := range resp.Objects {
 		o := obj.Object
 		o.Tile38ID = obj.ID
 		if haveFields {
 			o.Fields = make(map[string]int)
-			for fieldIdx, field := range sr.Fields {
+			for fieldIdx, field := range resp.Fields {
 				o.Fields[field] = obj.Fields[fieldIdx]
 			}
 		}
@@ -29,43 +34,7 @@ func (sr searchResponse) getObjects() []*Object {
 		objects[idx] = o
 	}
 
-	return objects
-}
-
-// Intersects searches a collection for objects that intersect a specified bounding area.
-func (client *Tile38Client) Intersects(key string, area SearchArea, opts ...SearchOption) ([]*Object, error) {
-	var resp searchResponse
-	cmd := buildSearchCommand("INTERSECTS", key, string(area), opts)
-	err := client.execute(cmd, &resp)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp.getObjects(), nil
-}
-
-// Within searches a collection for objects that are fully contained inside of a specified bounding area.
-func (client *Tile38Client) Within(key string, area SearchArea, opts ...SearchOption) ([]*Object, error) {
-	var resp searchResponse
-	cmd := buildSearchCommand("WITHIN", key, string(area), opts)
-	err := client.execute(cmd, &resp)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp.getObjects(), nil
-}
-
-// Nearby searches a collection for objects that are close to a specified point.
-func (client *Tile38Client) Nearby(key string, area NearbyArea, opts ...SearchOption) ([]*Object, error) {
-	var resp searchResponse
-	cmd := buildSearchCommand("NEARBY", key, string(area), opts)
-	err := client.execute(cmd, &resp)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp.getObjects(), nil
+	return objects, nil
 }
 
 func buildSearchCommand(cmd, key, area string, opts []SearchOption) string {
@@ -76,4 +45,19 @@ func buildSearchCommand(cmd, key, area string, opts []SearchOption) string {
 	}
 	sb.WriteString(" " + area)
 	return sb.String()
+}
+
+// Intersects searches a collection for objects that intersect a specified bounding area.
+func (client *Tile38Client) Intersects(key string, area SearchArea, opts ...SearchOption) ([]*Object, error) {
+	return client.searchObjects("INTERSECTS", key, string(area), opts)
+}
+
+// Within searches a collection for objects that are fully contained inside of a specified bounding area.
+func (client *Tile38Client) Within(key string, area SearchArea, opts ...SearchOption) ([]*Object, error) {
+	return client.searchObjects("WITHIN", key, string(area), opts)
+}
+
+// Nearby searches a collection for objects that are close to a specified point.
+func (client *Tile38Client) Nearby(key string, area NearbyArea, opts ...SearchOption) ([]*Object, error) {
+	return client.searchObjects("NEARBY", key, string(area), opts)
 }
