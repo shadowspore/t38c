b68eb5f9bebe58e58ad807c81c693a8936e2fdb3
lostpeer <yuiop1955@mail.ru>
2020-04-06 00:45:17 +0500

    fix: remove unused code

--- utils.go
+++ utils.go
@@ -1,15 +1,9 @@
 package t38c
 
 import (
-	"strconv"
-
 	geojson "github.com/paulmach/go.geojson"
 	"github.com/tidwall/gjson"
 )
-
-func floatToString(val float64) string {
-	return strconv.FormatFloat(val, 'f', 6, 64)
-}
 
 func unmarshalGeoJSON(data []byte) (interface{}, error) {
 	switch gjson.GetBytes(data, "type").String() {
