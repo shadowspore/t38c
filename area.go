453cd606b6fce994aec80b7590fe3d124cf5ba95
zerobounty <yuiop1955@mail.ru>
2020-04-06 00:46:00 +0500

    fix: remove linter warnings

--- area.go
+++ area.go
@@ -2,11 +2,13 @@ package t38c
 
 import geojson "github.com/paulmach/go.geojson"
 
+// Command struct
 type Command struct {
 	Name string
 	Args []interface{}
 }
 
+// NewCommand ...
 func NewCommand(name string, args ...interface{}) Command {
 	return Command{
 		Name: name,
