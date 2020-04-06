e7d8c1bfdbf8779d15a2e7a5cfca79a67edf76f8
lostpeer <yuiop1955@mail.ru>
2020-04-06 06:08:21 +0500

    feat: add searchBounds, searchHashes methods

--- search.go
+++ search.go
@@ -107,6 +107,86 @@ func (client *Tile38Client) searchIDs(cmd, key string, area Command, opts []Sear
 	return resp.IDs, nil
 }
 
+func (client *Tile38Client) searchBounds(cmd, key string, area Command, opts []SearchOption) ([]*BoundsObject, error) {
+	var resp struct {
+		Fields []string `json:"fields"`
+		Bounds []struct {
+			ID     string    `json:"id"`
+			Bounds Bounds    `json:"bounds"`
+			Fields []float64 `json:"fields"`
+		} `json:"bounds"`
+	}
+
+	opts = append(opts, SearchOption(NewCommand("BOUNDS")))
+	args := buildArgs(key, area, opts)
+	b, err := client.Execute(cmd, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(b, &resp); err != nil {
+		return nil, err
+	}
+
+	objects := make([]*BoundsObject, len(resp.Bounds))
+	haveFields := len(resp.Fields) > 0
+	for idx, obj := range resp.Bounds {
+		boundObj := &BoundsObject{}
+		boundObj.Tile38ID = obj.ID
+		boundObj.Bounds = obj.Bounds
+		if haveFields {
+			boundObj.Fields = make(map[string]float64)
+			for fieldIdx, field := range resp.Fields {
+				boundObj.Fields[field] = obj.Fields[fieldIdx]
+			}
+		}
+
+		objects[idx] = boundObj
+	}
+
+	return objects, nil
+}
+
+func (client *Tile38Client) searchHashes(cmd, key string, area Command, precision int, opts []SearchOption) ([]*HashObject, error) {
+	var resp struct {
+		Fields []string `json:"fields"`
+		Hashes []struct {
+			ID     string    `json:"id"`
+			Hash   string    `json:"hash"`
+			Fields []float64 `json:"fields"`
+		} `json:"hashes"`
+	}
+
+	opts = append(opts, SearchOption(NewCommand("HASHES", precision)))
+	args := buildArgs(key, area, opts)
+	b, err := client.Execute(cmd, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(b, &resp); err != nil {
+		return nil, err
+	}
+
+	objects := make([]*HashObject, len(resp.Hashes))
+	haveFields := len(resp.Fields) > 0
+	for idx, obj := range resp.Hashes {
+		hashObj := &HashObject{}
+		hashObj.Tile38ID = obj.ID
+		hashObj.Hash = obj.Hash
+		if haveFields {
+			hashObj.Fields = make(map[string]float64)
+			for fieldIdx, field := range resp.Fields {
+				hashObj.Fields[field] = obj.Fields[fieldIdx]
+			}
+		}
+
+		objects[idx] = hashObj
+	}
+
+	return objects, nil
+}
+
 func buildArgs(key string, area Command, opts []SearchOption) []interface{} {
 	var args []interface{}
 	args = append(args, key)
@@ -162,4 +242,34 @@ func (client *Tile38Client) WithinIDs(key string, area SearchArea, opts ...Searc
 // NearbyIDs ...
 func (client *Tile38Client) NearbyIDs(key string, area NearbyArea, opts ...SearchOption) ([]string, error) {
 	return client.searchIDs("NEARBY", key, Command(area), opts)
+}
+
+// IntersectsBounds ...
+func (client *Tile38Client) IntersectsBounds(key string, area SearchArea, opts ...SearchOption) ([]*BoundsObject, error) {
+	return client.searchBounds("INTERSECTS", key, Command(area), opts)
+}
+
+// WithinBounds ...
+func (client *Tile38Client) WithinBounds(key string, area SearchArea, opts ...SearchOption) ([]*BoundsObject, error) {
+	return client.searchBounds("WITHIN", key, Command(area), opts)
+}
+
+// NearbyBounds ...
+func (client *Tile38Client) NearbyBounds(key string, area NearbyArea, opts ...SearchOption) ([]*BoundsObject, error) {
+	return client.searchBounds("NEARBY", key, Command(area), opts)
+}
+
+// IntersectsHashes ...
+func (client *Tile38Client) IntersectsHashes(key string, area SearchArea, precision int, opts ...SearchOption) ([]*HashObject, error) {
+	return client.searchHashes("INTERSECTS", key, Command(area), precision, opts)
+}
+
+// WithinHashes ...
+func (client *Tile38Client) WithinHashes(key string, area SearchArea, precision int, opts ...SearchOption) ([]*HashObject, error) {
+	return client.searchHashes("WITHIN", key, Command(area), precision, opts)
+}
+
+// NearbyHashes ...
+func (client *Tile38Client) NearbyHashes(key string, area NearbyArea, precision int, opts ...SearchOption) ([]*HashObject, error) {
+	return client.searchHashes("NEARBY", key, Command(area), precision, opts)
 }
