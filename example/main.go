eae0cf09d0818c26645c88f565296395e4a75432
zerobounty <yuiop1955@mail.ru>
2020-04-08 22:03:50 +0500

    feat: new response types

--- example/main.go
+++ example/main.go
@@ -4,64 +4,26 @@ import (
 	"log"
 
 	"github.com/k0kubun/pp"
-	geojson "github.com/paulmach/go.geojson"
 	t38c "github.com/zerobounty/tile38-client"
 )
 
 func main() {
-	tile38, err := t38c.New(t38c.NewRadixPool("localhost:9851", 5))
+	tile38, err := t38c.New(t38c.NewRadixPool("localhost:9851", 5), t38c.Debug)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	tile38.Bounds("fleet")
-	feat := geojson.NewPolygonFeature([][][]float64{
-		[][]float64{
-			[]float64{1, 1},
-			[]float64{2, 1},
-			[]float64{1, 2},
-			[]float64{1, 1},
-			[]float64{1, 1},
-		},
-	})
-
-	feat.SetProperty("sadness", 999)
-	if err := tile38.Set("fleet", "truck01", t38c.SetFeature(feat),
-		t38c.SetField("speed", 10),
-	); err != nil {
-		log.Fatal(err)
-	}
-
-	obs, err := tile38.Intersects("fleet", t38c.AreaCircle(0, 0, 999999999))
-	if err != nil {
-		log.Fatal(err)
-	}
-	pp.Println(obs)
-
-	ob, err := tile38.GetPoint("fleet", "truck1")
-	if err != nil {
-		log.Fatal(err)
-	}
-	pp.Println(ob)
-
-	tile38.Set("fleet", "truck2", t38c.SetGeometry(feat.Geometry))
-	obe, err := tile38.Get("fleet", "truck2", false)
-	if err != nil {
-		log.Fatal(err)
-	}
-	pp.Println(obe.GeoJSON)
-
-	pts, err := tile38.WithinPoints("fleet", t38c.AreaCircle(2, 2, 99999))
+	res, err := tile38.Nearby("fleet", t38c.NearbyPoint(0, 0, 9999999))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	pp.Println(pts)
+	pp.Println(res)
 
-	hashes, err := tile38.NearbyBounds("fleet", t38c.NearbyPoint(0, 0, 999999))
+	res1, err := tile38.Get("fleet", "truck1", true)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	pp.Println(hashes)
+	pp.Println(res1)
 }
