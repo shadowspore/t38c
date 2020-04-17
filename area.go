d74faf1938580f132c043d59dbd6699ff06bef67
lostpeer <yuiop1955@mail.ru>
2020-04-17 21:07:10 +0500

    fix: use functions instead var

--- area.go
+++ area.go
@@ -9,108 +9,104 @@ import (
 // SearchArea ...
 type SearchArea Command
 
-var (
-	// Get any object that already exists in the database.
-	Get = func(objectID string) SearchArea {
-		return SearchArea(NewCommand("GET", objectID))
-	}
+// Get any object that already exists in the database.
+func Get(objectID string) SearchArea {
+	return SearchArea(NewCommand("GET", objectID))
+}
 
-	// AreaBounds - a minimum bounding rectangle.
-	AreaBounds = func(minlat, minlon, maxlat, maxlon float64) SearchArea {
-		return SearchArea(NewCommand("BOUNDS", floatString(minlat), floatString(minlon), floatString(maxlat), floatString(maxlon)))
-	}
+// AreaBounds - a minimum bounding rectangle.
+func AreaBounds(minlat, minlon, maxlat, maxlon float64) SearchArea {
+	return SearchArea(NewCommand("BOUNDS", floatString(minlat), floatString(minlon), floatString(maxlat), floatString(maxlon)))
+}
 
-	// AreaFeatureCollection - GeoJSON Feature Collection object.
-	AreaFeatureCollection = func(fc *geojson.FeatureCollection) SearchArea {
-		// TODO: handle error?
-		b, _ := fc.MarshalJSON()
-		return SearchArea(NewCommand("OBJECT", string(b)))
-	}
+// AreaFeatureCollection - GeoJSON Feature Collection object.
+func AreaFeatureCollection(fc *geojson.FeatureCollection) SearchArea {
+	// TODO: handle error?
+	b, _ := fc.MarshalJSON()
+	return SearchArea(NewCommand("OBJECT", string(b)))
+}
 
-	// AreaFeature - GeoJSON Feature object.
-	AreaFeature = func(ft *geojson.Feature) SearchArea {
-		// TODO: handle error?
-		b, _ := ft.MarshalJSON()
-		return SearchArea(NewCommand("OBJECT", string(b)))
-	}
+// AreaFeature - GeoJSON Feature object.
+func AreaFeature(ft *geojson.Feature) SearchArea {
+	// TODO: handle error?
+	b, _ := ft.MarshalJSON()
+	return SearchArea(NewCommand("OBJECT", string(b)))
+}
 
-	// AreaGeometry - GeoJSON Geometry object.
-	AreaGeometry = func(gm *geojson.Geometry) SearchArea {
-		// TODO: handle error?
-		b, _ := gm.MarshalJSON()
-		return SearchArea(NewCommand("OBJECT", string(b)))
-	}
+// AreaGeometry - GeoJSON Geometry object.
+func AreaGeometry(gm *geojson.Geometry) SearchArea {
+	// TODO: handle error?
+	b, _ := gm.MarshalJSON()
+	return SearchArea(NewCommand("OBJECT", string(b)))
+}
 
-	// AreaCircle - a circle with the specified center and radius.
-	AreaCircle = func(lat, lon, meters float64) SearchArea {
-		return SearchArea(NewCommand("CIRCLE", floatString(lat), floatString(lon), floatString(meters)))
-	}
+// AreaCircle - a circle with the specified center and radius.
+func AreaCircle(lat, lon, meters float64) SearchArea {
+	return SearchArea(NewCommand("CIRCLE", floatString(lat), floatString(lon), floatString(meters)))
+}
 
-	// AreaTile - an XYZ Tile.
-	AreaTile = func(x, y, z int) SearchArea {
-		return SearchArea(NewCommand("TILE", strconv.Itoa(x), strconv.Itoa(y), strconv.Itoa(z)))
-	}
+// AreaTile - an XYZ Tile.
+func AreaTile(x, y, z int) SearchArea {
+	return SearchArea(NewCommand("TILE", strconv.Itoa(x), strconv.Itoa(y), strconv.Itoa(z)))
+}
 
-	// AreaQuadkey - a QuadKey.
-	AreaQuadkey = func(quadkey string) SearchArea {
-		return SearchArea(NewCommand("QUADKEY", quadkey))
-	}
+// AreaQuadkey - a QuadKey.
+func AreaQuadkey(quadkey string) SearchArea {
+	return SearchArea(NewCommand("QUADKEY", quadkey))
+}
 
-	// AreaHash - a Geohash.
-	AreaHash = func(hash string) SearchArea {
-		return SearchArea(NewCommand("HASH", hash))
-	}
-)
+// AreaHash - a Geohash.
+func AreaHash(hash string) SearchArea {
+	return SearchArea(NewCommand("HASH", hash))
+}
 
 // SetArea ...
 type SetArea Command
 
-var (
-	// SetPoint set a simple point in latitude, longitude.
-	SetPoint = func(lat, lon float64) SetArea {
-		return SetArea(NewCommand("POINT", floatString(lat), floatString(lon)))
-	}
+// SetPoint set a simple point in latitude, longitude.
+func SetPoint(lat, lon float64) SetArea {
+	return SetArea(NewCommand("POINT", floatString(lat), floatString(lon)))
+}
 
-	// SetPointZ - a point with Z coordinate.
-	// This is application specific such as elevation, or a timestamp, etc.
-	SetPointZ = func(lat, lon, z float64) SetArea {
-		return SetArea(NewCommand("POINT", floatString(lat), floatString(lon), floatString(z)))
-	}
+// SetPointZ - a point with Z coordinate.
+// This is application specific such as elevation, or a timestamp, etc.
+func SetPointZ(lat, lon, z float64) SetArea {
+	return SetArea(NewCommand("POINT", floatString(lat), floatString(lon), floatString(z)))
+}
 
-	// SetBounds - a bounding box consists of two points.
-	// The first being the southwestern most point and the second is the northeastern most point.
-	SetBounds = func(lat1, lon1, lat2, lon2 float64) SetArea {
-		return SetArea(NewCommand("BOUNDS", floatString(lat1), floatString(lon1), floatString(lat2), floatString(lon2)))
-	}
+// SetBounds - a bounding box consists of two points.
+// The first being the southwestern most point and the second is the northeastern most point.
+func SetBounds(lat1, lon1, lat2, lon2 float64) SetArea {
+	return SetArea(NewCommand("BOUNDS", floatString(lat1), floatString(lon1), floatString(lat2), floatString(lon2)))
+}
 
-	// SetFeatureCollection - set GeoJSON Feature Collection object.
-	SetFeatureCollection = func(fc *geojson.FeatureCollection) SetArea {
-		b, _ := fc.MarshalJSON()
-		return SetArea(NewCommand("OBJECT", string(b)))
-	}
+// SetFeatureCollection - set GeoJSON Feature Collection object.
+func SetFeatureCollection(fc *geojson.FeatureCollection) SetArea {
+	b, _ := fc.MarshalJSON()
+	return SetArea(NewCommand("OBJECT", string(b)))
+}
 
-	// SetFeature - set GeoJSON Feature object.
-	SetFeature = func(ft *geojson.Feature) SetArea {
-		b, _ := ft.MarshalJSON()
-		return SetArea(NewCommand("OBJECT", string(b)))
-	}
+// SetFeature - set GeoJSON Feature object.
+func SetFeature(ft *geojson.Feature) SetArea {
+	b, _ := ft.MarshalJSON()
+	return SetArea(NewCommand("OBJECT", string(b)))
+}
 
-	// SetGeometry - set GeoJSON Geometry object.
-	SetGeometry = func(gm *geojson.Geometry) SetArea {
-		b, _ := gm.MarshalJSON()
-		return SetArea(NewCommand("OBJECT", string(b)))
-	}
+// SetGeometry - set GeoJSON Geometry object.
+func SetGeometry(gm *geojson.Geometry) SetArea {
+	b, _ := gm.MarshalJSON()
+	return SetArea(NewCommand("OBJECT", string(b)))
+}
 
-	// SetHash - A geohash is a convenient way of expressing a location (anywhere in the world)
-	// using a short alphanumeric string, with greater precision obtained with longer strings.
-	SetHash = func(hash string) SetArea {
-		return SetArea(NewCommand("HASH", hash))
-	}
+// SetHash - A geohash is a convenient way of expressing a location (anywhere in the world)
+// using a short alphanumeric string, with greater precision obtained with longer strings.
+func SetHash(hash string) SetArea {
+	return SetArea(NewCommand("HASH", hash))
+}
 
-	// SetString - It’s possible to set a raw string.
-	// The value of a string type can be plain text or a series of raw bytes.
-	// To retrieve a string value you can use GET, SCAN, or SEARCH.
-	SetString = func(str string) SetArea {
-		return SetArea(NewCommand("STRING", str))
-	}
-)
+// SetString - It’s possible to set a raw string.
+// The value of a string type can be plain text or a series of raw bytes.
+// To retrieve a string value you can use GET, SCAN, or SEARCH.
+func SetString(str string) SetArea {
+	return SetArea(NewCommand("STRING", str))
+}
--- options.go
+++ options.go
@@ -19,65 +19,54 @@ var (
 	// It can only be used with these area formats: BOUNDS, TILE, QUADKEY, HASH.
 	Clip = SearchOption(NewCommand("CLIP"))
 
-	// Cursor is used to iterate though many objects from the search results.
-	// An iteration begins when the CURSOR is set to Zero or not included with the request,
-	// and completes when the cursor returned by the server is Zero.
-	Cursor = func(start int) SearchOption {
-		return SearchOption(NewCommand("CURSOR", strconv.Itoa(start)))
-	}
-
-	// Limit can be used to limit the number of objects returned for a single search request.
-	Limit = func(count int) SearchOption {
-		return SearchOption(NewCommand("LIMIT", strconv.Itoa(count)))
-	}
-
-	// Sparse will distribute the results of a search evenly across the requested area.
-	Sparse = func(n int) SearchOption {
-		return SearchOption(NewCommand("SPARSE", strconv.Itoa(n)))
-	}
-
-	// Where allows for filtering out results based on field values.
-	Where = func(field string, min, max float64) SearchOption {
-		return SearchOption(NewCommand("WHERE", field, floatString(min), floatString(max)))
-	}
-
-	// Wherein is similar to Where except that it checks whether the object’s field value is in a given list.
-	Wherein = func(field string, values ...float64) SearchOption {
-		var args []string
-		args = append(args, strconv.Itoa(len(values)))
-		for _, val := range values {
-			args = append(args, floatString(val))
-		}
-
-		return SearchOption(NewCommand("WHEREIN", args...))
-	}
-
-	// Match is similar to WHERE except that it works on the object id instead of fields.
-	// There can be multiple MATCH options in a single search.
-	// The MATCH value is a simple glob pattern.
-	Match = func(pattern string) SearchOption {
-		return SearchOption(NewCommand("MATCH", pattern))
-	}
-
 	// Distance allows to return between objects. Only for NEARBY command.
 	Distance = SearchOption(NewCommand("DISTANCE"))
 )
+
+// Cursor is used to iterate though many objects from the search results.
+// An iteration begins when the CURSOR is set to Zero or not included with the request,
+// and completes when the cursor returned by the server is Zero.
+func Cursor(start int) SearchOption {
+	return SearchOption(NewCommand("CURSOR", strconv.Itoa(start)))
+}
+
+// Limit can be used to limit the number of objects returned for a single search request.
+func Limit(count int) SearchOption {
+	return SearchOption(NewCommand("LIMIT", strconv.Itoa(count)))
+}
+
+// Sparse will distribute the results of a search evenly across the requested area.
+func Sparse(n int) SearchOption {
+	return SearchOption(NewCommand("SPARSE", strconv.Itoa(n)))
+}
+
+// Where allows for filtering out results based on field values.
+func Where(field string, min, max float64) SearchOption {
+	return SearchOption(NewCommand("WHERE", field, floatString(min), floatString(max)))
+}
+
+// Wherein is similar to Where except that it checks whether the object’s field value is in a given list.
+func Wherein(field string, values ...float64) SearchOption {
+	var args []string
+	args = append(args, strconv.Itoa(len(values)))
+	for _, val := range values {
+		args = append(args, floatString(val))
+	}
+
+	return SearchOption(NewCommand("WHEREIN", args...))
+}
+
+// Match is similar to WHERE except that it works on the object id instead of fields.
+// There can be multiple MATCH options in a single search.
+// The MATCH value is a simple glob pattern.
+func Match(pattern string) SearchOption {
+	return SearchOption(NewCommand("MATCH", pattern))
+}
 
 // SetOption ...
 type SetOption Command
 
 var (
-	// Field are extra data which belongs to an object.
-	// A field is always a double precision floating point.
-	// There is no limit to the number of fields that an object can have.
-	Field = func(name string, value float64) SetOption {
-		return SetOption(NewCommand("FIELD", name, floatString(value)))
-	}
-
-	// Expiration set the specified expire time, in seconds.
-	Expiration = func(seconds int) SetOption {
-		return SetOption(NewCommand("EX", strconv.Itoa(seconds)))
-	}
 
 	// IfNotExists only set the object if it does not already exist.
 	IfNotExists = SetOption(NewCommand("NX"))
@@ -85,3 +74,15 @@ var (
 	// IfExists only set the object if it already exist.
 	IfExists = SetOption(NewCommand("XX"))
 )
+
+// Field are extra data which belongs to an object.
+// A field is always a double precision floating point.
+// There is no limit to the number of fields that an object can have.
+func Field(name string, value float64) SetOption {
+	return SetOption(NewCommand("FIELD", name, floatString(value)))
+}
+
+// Expiration set the specified expire time, in seconds.
+func Expiration(seconds int) SetOption {
+	return SetOption(NewCommand("EX", strconv.Itoa(seconds)))
+}
