586ead4937853cb4f6e17a6b68482e3640415252
rs <yuiop1955@mail.ru>
2020-05-23 01:13:15 +0500

    rename search query structs

--- search.go
+++ search.go
@@ -1,26 +1,26 @@
 package t38c
 
 // Within searches a collection for objects that are fully contained inside of a specified bounding area.
-func (client *Client) Within(key string) SearchAreaSelector {
-	return newSearchAreaSelector(client, "WITHIN", key)
+func (client *Client) Within(key string) InwAreaSelector {
+	return newInwAreaSelector(client, "WITHIN", key)
 }
 
 // Intersects searches a collection for objects that intersect a specified bounding area.
-func (client *Client) Intersects(key string) SearchAreaSelector {
-	return newSearchAreaSelector(client, "INTERSECTS", key)
+func (client *Client) Intersects(key string) InwAreaSelector {
+	return newInwAreaSelector(client, "INTERSECTS", key)
 }
 
 // Nearby command searches a collection for objects that are close to a specified point.
 // The KNN algorithm is used instead of the standard overlap+Haversine algorithm,
 // sorting the results in order of ascending distance from that point, i.e., nearest first.
-func (client *Client) Nearby(key string, lat, lon, meters float64) SearchQueryBuilder {
+func (client *Client) Nearby(key string, lat, lon, meters float64) InwQueryBuilder {
 	area := NewCommand("POINT", floatString(lat), floatString(lon), floatString(meters))
-	return newSearchQueryBuilder(client, "NEARBY", key, area)
+	return newInwQueryBuilder(client, "NEARBY", key, area)
 }
 
 // Search iterates though a key’s string values.
-func (client *Client) Search(key string) SearchQueryBuilder {
-	return SearchQueryBuilder{
+func (client *Client) Search(key string) InwQueryBuilder {
+	return InwQueryBuilder{
 		client: client,
 		cmd:    "SEARCH",
 		key:    key,
@@ -28,8 +28,8 @@ func (client *Client) Search(key string) SearchQueryBuilder {
 }
 
 // Scan incrementally iterates though a key.
-func (client *Client) Scan(key string) SearchQueryBuilder {
-	return SearchQueryBuilder{
+func (client *Client) Scan(key string) InwQueryBuilder {
+	return InwQueryBuilder{
 		client: client,
 		cmd:    "SCAN",
 		key:    key,
