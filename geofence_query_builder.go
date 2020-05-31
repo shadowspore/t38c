12f0ffee9947c9f86aea417b7f96f01840123aaa
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2020-05-31 17:13:31 +0430

    fixes a minor bug

--- geofence_query_builder.go
+++ geofence_query_builder.go
@@ -195,7 +195,7 @@ func (query GeofenceQueryBuilder) Wherein(field string, values ...float64) Geofe
 // and (2) has the FIELDS global with the Lua table of the iterated object’s fields.
 func (query GeofenceQueryBuilder) WhereEval(script string, args ...string) GeofenceQueryBuilder {
 	cmd := NewCommand("WHEREEVAL", append([]string{script}, args...)...)
-	query.opts = append(query.opts, cmd)
+	query.searchOpts = append(query.searchOpts, cmd)
 	return query
 }
 
@@ -209,7 +209,7 @@ func (query GeofenceQueryBuilder) WhereEval(script string, args ...string) Geofe
 // and (2) has the FIELDS global with the Lua table of the iterated object’s fields.
 func (query GeofenceQueryBuilder) WhereEvalSHA(sha string, args ...string) GeofenceQueryBuilder {
 	cmd := NewCommand("WHEREEVALSHA", append([]string{sha}, args...)...)
-	query.opts = append(query.opts, cmd)
+	query.searchOpts = append(query.searchOpts, cmd)
 	return query
 }
 
