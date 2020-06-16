776e86196fcaab99a934eda22a1494371278c78d
rs <yuiop1955@mail.ru>
2020-06-16 23:14:22 +0500

    test: new tests

--- query_test.go
+++ query_test.go
@@ -1,125 +1,118 @@
 package t38c
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
 
-func TestInwQueryBuilder(t *testing.T) {
+func TestSearch(t *testing.T) {
 	search := &Search{}
 	tests := []struct {
-		Query    InwQueryBuilder
+		Cmd      *tileCmd
 		Expected string
 	}{
 		{
-			Query: search.Nearby("fleet", 10, 20, 30).
+			Cmd: search.Nearby("fleet", 10, 20, 30).
 				Where("speed", 10, 20).
 				Wherein("speed", 10, 20, 30).
 				Match("abc*").
 				Cursor(10).
 				Format(FormatIDs).
-				Limit(5),
+				Limit(5).toCmd(),
 			Expected: "NEARBY fleet WHERE speed 10 20 WHEREIN speed 3 10 20 30 MATCH abc* CURSOR 10 LIMIT 5 IDS POINT 10 20 30",
 		},
 		{
-			Query: search.Intersects("fleet").
+			Cmd: search.Intersects("fleet").
 				Tile(10, 20, 30).
-				Match("abc*"),
+				Match("abc*").toCmd(),
 			Expected: "INTERSECTS fleet MATCH abc* TILE 10 20 30",
 		},
 	}
 
 	for _, test := range tests {
-		cmd := *test.Query.toCmd()
-		actual := append([]string{cmd.Name}, cmd.Args...)
-		expected := strings.Split(test.Expected, " ")
-		if len(actual) != len(expected) {
-			t.Fatalf("not equal: bad length (%d, %d)\n"+
-				"expected: %s\n"+
-				"actual  : %s\n", len(expected), len(actual), expected, actual)
-		}
-		for i := 0; i < len(actual); i++ {
-			if actual[i] != expected[i] {
-				t.Fatalf("not equal:\n"+
-					"expected: %s\n"+
-					"actual  : %s\n", expected, actual)
-			}
+		err := compare(test.Cmd, test.Expected)
+		if err != nil {
+			t.Fatal(err)
 		}
 	}
 }
 
-func TestGeofenceQueryBuilder(t *testing.T) {
+func TestGeofence(t *testing.T) {
 	geofence := &Geofence{}
 	tests := []struct {
-		Query    GeofenceQueryBuilder
+		Cmd      *tileCmd
 		Expected string
 	}{
 		{
-			Query: geofence.Nearby("fleet", 10, 20, 30).
+			Cmd: geofence.Nearby("fleet", 10, 20, 30).
 				Actions(Enter, Exit, Cross).
 				Clip().
 				Commands(Set, Del).
 				Cursor(5).
-				Format(FormatHashes(5)),
+				Format(FormatHashes(5)).toCmd(),
 			Expected: "NEARBY fleet CLIP CURSOR 5 FENCE DETECT enter,exit,cross COMMANDS set,del HASHES 5 POINT 10 20 30",
 		},
 		{
-			Query: geofence.Roam("agent", "target", "*", 100).
+			Cmd: geofence.Roam("agent", "target", "*", 100).
 				Distance().
-				Wherein("price", 20, 30),
+				Wherein("price", 20, 30).toCmd(),
 			Expected: "NEARBY agent DISTANCE WHEREIN price 2 20 30 FENCE ROAM target * 100",
 		},
 	}
 
 	for _, test := range tests {
-		cmd := *test.Query.toCmd()
-		actual := append([]string{cmd.Name}, cmd.Args...)
-		expected := strings.Split(test.Expected, " ")
-		if len(actual) != len(expected) {
-			t.Fatalf("not equal: bad length (%d, %d)\n"+
-				"expected: %s\n"+
-				"actual  : %s\n", len(expected), len(actual), expected, actual)
-		}
-		for i := 0; i < len(actual); i++ {
-			if actual[i] != expected[i] {
-				t.Fatalf("not equal:\n"+
-					"expected: %s\n"+
-					"actual  : %s\n", expected, actual)
-			}
+		err := compare(test.Cmd, test.Expected)
+		if err != nil {
+			t.Fatal(err)
 		}
 	}
 }
 
-func TestSetQueryBuilder(t *testing.T) {
+func TestKeys(t *testing.T) {
 	keys := &Keys{}
 	tests := []struct {
-		Query    SetQueryBuilder
+		Cmd      *tileCmd
 		Expected string
 	}{
 		{
-			Query: keys.Set("agent", "47").
+			Cmd: keys.Set("agent", "47").
 				PointZ(0, 0, -20).
 				Field("age", 55).
-				Expiration(60 * 60 * 24 * 365),
+				Expiration(60 * 60 * 24 * 365).toCmd(),
 			Expected: "SET agent 47 EX 31536000 FIELD age 55 POINT 0 0 -20",
+		},
+		{
+			Cmd: keys.FSet("agent", "47").
+				Field("cash", 100500).
+				IfExists().toCmd(),
+			Expected: "FSET agent 47 XX cash 100500",
 		},
 	}
 
 	for _, test := range tests {
-		cmd := *test.Query.toCmd()
-		actual := append([]string{cmd.Name}, cmd.Args...)
-		expected := strings.Split(test.Expected, " ")
-		if len(actual) != len(expected) {
-			t.Fatalf("not equal: bad length (%d, %d)\n"+
-				"expected: %s\n"+
-				"actual  : %s\n", len(expected), len(actual), expected, actual)
-		}
-		for i := 0; i < len(actual); i++ {
-			if actual[i] != expected[i] {
-				t.Fatalf("not equal:\n"+
-					"expected: %s\n"+
-					"actual  : %s\n", expected, actual)
-			}
+		err := compare(test.Cmd, test.Expected)
+		if err != nil {
+			t.Fatal(err)
 		}
 	}
+}
+
+func compare(cmd *tileCmd, expectedCmd string) error {
+	actual := append([]string{cmd.Name}, cmd.Args...)
+	expected := strings.Split(expectedCmd, " ")
+	if len(actual) != len(expected) {
+		return fmt.Errorf("not equal: bad length (%d, %d)\n"+
+			"expected: %s\n"+
+			"actual  : %s\n", len(expected), len(actual), expected, actual)
+	}
+
+	for i := 0; i < len(actual); i++ {
+		if actual[i] != expected[i] {
+			return fmt.Errorf("not equal:\n"+
+				"expected: %s\n"+
+				"actual  : %s\n", expected, actual)
+		}
+	}
+	return nil
 }
